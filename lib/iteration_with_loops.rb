89521d57445101d44575243d761a728a64877a5b
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-18 23:04:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def find_min_in_nested_arrays(array)
     min_temprature = 0 
     temporary_min = 
     while min_temprature < array[row_index].count do
-      if array[row_index][min_temprature] < temporary_min
+      
       if temporary_min == nil || array[row_index][min_temprature] < temporary_min
       	temporary_min
       end
